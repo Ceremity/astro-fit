6aae34f4a1864378d9bbc57b2cbdbfdf85f98e34
Shan Memon <shankmemon@gmail.com>
2018-07-18 00:37:56 -0500

    Allowed UI Sound Functionality

---
+++ Astro Fit/Assets/Scripts/UI/MenuSoundWaitSceneLoad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class MenuSoundWaitSceneLoad : MonoBehaviour {
+
+	public UnityEngine.Events.UnityEvent RunAfterSound;
+
+	private AudioSource audio;
+
+	private void Start() {
+		audio = GetComponent<AudioSource>();
+	}
+
+
+	public void PlaySound() {
+		if(audio != null && audio.clip != null)
+			StartCoroutine(DelayedLoad());
+	}
+
+	IEnumerator DelayedLoad() {
+		//Play the clip once
+		audio.Play();
+
+		//Wait until clip finish playing
+		yield return new WaitForSeconds(audio.clip.length);
+
+		//Load scene here
+		RunAfterSound.Invoke();
+
+	}
+}

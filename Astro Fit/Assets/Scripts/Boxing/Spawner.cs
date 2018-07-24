a087043a950bbf6dee97c96ae2d9f1fdadd59226
CeremityPaul <paul.rice@ceremity.com>
2018-07-24 16:04:28 -0500

    FPS Optimize (got rid of snow)

--- Astro Fit/Assets/Scripts/Boxing/Spawner.cs
+++ Astro Fit/Assets/Scripts/Boxing/Spawner.cs
@@ -8,17 +8,20 @@ public class Spawner : MonoBehaviour {
     private float size = 1f;
     [SerializeField]
     private GameObject breakablePrefab;
-    
+    [SerializeField]
+    private float xDistance = 0.13f;
+    [SerializeField]
+    private float yDistanceHigh = 1.45f;
+    [SerializeField]
+    private float yDistanceLow = 0.97f;
 
-	void Update () {
-        //if (Input.GetKeyDown(KeyCode.Alpha1))
-        //{
-        //    for(int i = 1; i <= 9; i++)
-        //    {
-        //        Spawn(i);
-        //    }
-        //}
-            
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            for (int i = 0; i <= 3; i++) {
+                Spawn(i);
+            }
+        }
+
         //if (Input.GetKeyDown(KeyCode.Alpha2))
         //    Spawn(2);
         //if (Input.GetKeyDown(KeyCode.Alpha3))
@@ -39,11 +42,18 @@ public class Spawner : MonoBehaviour {
 
     public void Spawn(int key)
     {
-        int row = (key - 1) % 3;
-        int col = (key - 1) / 3;
-        float x = row * size - size;
-        float y = col * size + size;
-        Instantiate(breakablePrefab, transform.position + new Vector3(x, y, 0), Quaternion.identity);
+        Vector3 spawnPosition = new Vector3();
+        switch(key) {
+            case 0: spawnPosition = new Vector3(-xDistance, yDistanceHigh, 0); break;
+            case 1: spawnPosition = new Vector3(xDistance,yDistanceHigh, 0); break;
+            case 2: spawnPosition = new Vector3(-xDistance, yDistanceLow, 0); break;
+            case 3: spawnPosition = new Vector3(xDistance, yDistanceLow, 0); break;
+        }
+        //int row = (key - 1) % 3;
+        //int col = (key - 1) / 3;
+        //float x = row * size - size;
+        //float y = col * size + size;
+        Instantiate(breakablePrefab, transform.position + spawnPosition, Quaternion.identity);
 
     }
 
